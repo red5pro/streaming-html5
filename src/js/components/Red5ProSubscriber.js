d09c6e8548f908a04fd20889d05b9c1979de866a
bustardcelly <bustardcelly@gmail.com>
2016-10-10 20:57:20 -0400

    refactor out of subscriber to own base component.

---
+++ src/js/components/Red5ProSubscriber.js
@@ -0,0 +1,145 @@
+/* global red5prosdk */
+import React from 'react'
+// import red5prosdk from 'red5pro-sdk'
+import { PropTypes } from 'react'
+
+const defaultConfiguration = {
+  protocol: 'ws',
+  port: 8081,
+  app: 'live',
+  bandwidth: {
+    audio: 50,
+    video: 256,
+    data: 30 * 1000 * 1000
+  }
+}
+
+class Red5ProSubscriber extends React.Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      view: undefined,
+      subscriber: undefined
+    }
+  }
+
+  onSubscribeFail (message) {
+    console.error(message)
+  }
+
+  onSubscribeSuccess () {
+  }
+
+  onUnsubscribeFail (message) {
+    console.error(message)
+  }
+
+  onUnsubscribeSuccess () {
+  }
+
+  notifySubscriberEstablished (subscriber, view) {
+    if (this.props.onSubscriberEstablished) {
+      this.props.onSubscriberEstablished(subscriber, view)
+    }
+  }
+
+  subscribe () {
+    const comp = this
+    const view = new red5prosdk.PlaybackView('red5pro-subscriber-video')
+    const subscriber = new red5prosdk.RTCSubscriber()
+
+    const origAttachStream = view.attachStream.bind(view)
+    view.attachStream = (stream, autoplay) => {
+      origAttachStream(stream, autoplay)
+      view.attachStream = origAttachStream
+    }
+
+    view.attachSubscriber(subscriber)
+
+    const config = Object.assign(defaultConfiguration, this.props.configuration)
+    config.port = config.rtcport || config.port
+    config.streamName = this.props.streamName || this.config.streamName
+    config.subscriptionId = 'subscriber-' + Math.floor(Math.random() * 0x10000).toString(16)
+
+    subscriber.init(config)
+      .then(player => {
+        comp.setState(state => {
+          state.view = view
+          state.subscriber = subscriber
+        })
+        return player.play()
+      })
+      .then(() => {
+        comp.onSubscribeSuccess()
+        comp.notifySubscriberEstablished(subscriber, view)
+      })
+      .catch(error => {
+        const jsonError = typeof error === 'string' ? error : JSON.stringify(error, null, 2)
+        comp.onSubscribeFail(`[Red5ProSubscriber] :: Error - ${jsonError}`)
+    })
+
+  }
+
+  unsubscribe () {
+    const comp = this;
+    const view = comp.state.view;
+    const subscriber = comp.state.subscriber;
+    if (subscriber) {
+      subscriber.stop()
+        .then(() => {
+          view.view.src = ''
+          subscriber.setView(undefined)
+          comp.setState(state => {
+            state.view = undefined
+            state.subscriber = undefined
+          })
+          comp.onUnsubscribeSuccess()
+        })
+        .catch(error => {
+          const jsonError = typeof error === 'string' ? error : JSON.stringify(error, null, 2)
+          comp.onUnsubscribeFail(`[Red5ProSubscriber] :: Unmount Error = ${jsonError}`)
+        })
+    }  }
+
+  componentDidMount () {
+    this.subscribe()
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe()
+  }
+
+  render () {
+    return (
+      <div ref={c => this._videoContainer = c}
+        id="red5pro-subscriber-video-container">
+        <video ref={c => this._red5ProSubscriber = c}
+          id="red5pro-subscriber-video"
+          controls={this.props.showControls}
+          autoplay={this.props.autoPlay}>
+        </video>
+      </div>
+    )
+  }
+
+}
+
+Red5ProSubscriber.propTypes = {
+  autoPlay: PropTypes.boolean,
+  showControls: PropTypes.boolean,
+  streamName: PropTypes.string.isRequired,
+  settings: PropTypes.object.isRequired,
+  onBackClick: PropTypes.func.isRequired,
+  onSubscriberEstablished: PropTypes.func
+}
+
+Red5ProSubscriber.defaultProps = {
+  autoPlay: true,
+  showControls: true,
+  streamName: undefined,
+  consfiguration: defaultConfiguration
+}
+
+export default Red5ProSubscriber
+
--- src/js/components/test/subscribe/SubscriberTest.js
+++ src/js/components/test/subscribe/SubscriberTest.js
@@ -1,7 +1,6 @@
-/* global red5prosdk */
 import React from 'react'
-// import red5prosdk from 'red5pro-sdk'
 import { PropTypes } from 'react'
+import Red5ProSubscriber from '../../Red5ProSubscriber' // eslint-disable-line no-unused-vars
 import BackLink from '../../BackLink' // eslint-disable-line no-unused-vars
 
 class SubscriberTest extends React.Component {
@@ -9,90 +8,12 @@ class SubscriberTest extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      view: undefined,
-      subscriber: undefined,
       status: 'On Hold.'
     }
   }
 
-  subscribe () {
-    const comp = this
-    const view = new red5prosdk.PlaybackView('red5pro-subscriber')
-    const subscriber = new red5prosdk.RTCSubscriber()
-    const iceServers = [{urls: 'stun:stun2.l.google.com:19302'}]
-
-    const origAttachStream = view.attachStream.bind(view)
-    view.attachStream = (stream, autoplay) => {
-      comp.setState(state => {
-        state.status = 'Subscribed. They\'re Live!'
-      })
-      origAttachStream(stream, autoplay)
-      view.attachStream = origAttachStream
-    }
-
-    comp.setState(state => {
-      state.status = 'Establishing connection...'
-    })
-    view.attachSubscriber(subscriber)
-    subscriber.init({
-      protocol: 'ws',
-      host: this.props.settings.host,
-      port: this.props.settings.rtcport,
-      app: this.props.settings.context,
-      subscriptionId: 'subscriber-' + Math.floor(Math.random() * 0x10000).toString(16),
-      streamName: this.props.settings.stream1,
-      iceServers: iceServers,
-      bandwidth: {
-        audio: 50,
-        video: 256,
-        data: 30 * 1000 * 1000
-      }
-    })
-    .then(player => {
-      comp.setState(state => {
-        state.view = view
-        state.subscriber = subscriber
-      })
-      comp.setState(state => {
-        state.status = 'Negotating connection...'
-      })
-      return player.play()
-    })
-    .then(() => {
-      comp.setState(state => {
-        state.status = 'Requesting stream for playback...'
-      })
-    })
-    .catch(error => {
-      const jsonError = typeof error === 'string' ? error : JSON.stringify(error, null, 2)
-      console.error(`[SubscriberTest] :: Error - ${jsonError}`)
-    })
-
-  }
-
-  componentDidMount () {
-    this.subscribe()
-  }
-
-  componentWillUnmount() {
-    const comp = this;
-    const view = comp.state.view;
-    const subscriber = comp.state.subscriber;
-    if (subscriber) {
-      subscriber.stop()
-        .then(() => {
-          view.view.src = ''
-          subscriber.setView(undefined)
-          comp.setState(state => {
-            state.view = undefined
-            state.subscriber = undefined
-          })
-        })
-        .catch(error => {
-          const jsonError = typeof error === 'string' ? error : JSON.stringify(error, null, 2)
-          console.error(`[SubscriberTest] :: Unmount Error = ${jsonError}`)
-        })
-    }
+  subscriberEstablished (subscriber, view) {
+    console.log(`subscriber: ${subscriber}, ${view}`)
   }
 
   render () {
@@ -107,14 +28,13 @@ class SubscriberTest extends React.Component {
         <hr />
         <h2 className="centered"><em>stream</em>: {this.props.settings.stream1}</h2>
         <p className="centered subscriber-status-field">STATUS: {this.state.status}</p>
-        <div ref={c => this._videoContainer = c}
-          id="video-container"
-          className="centered">
-          <video ref={c => this._red5ProSubscriber = c}
-            id="red5pro-subscriber"
-            style={videoStyle}
-            controls autoplay></video>
-        </div>
+        <Red5ProSubscriber className="centered" style={videoStyle}
+          configuration={this.props.settings}
+          streamName={this.props.settings.stream1}
+          autoPlay={true}
+          showControls={true}
+          onSubscriberEstablished={this.subscriberEstablished.bind(this)}
+        />
       </div>
     )
   }
