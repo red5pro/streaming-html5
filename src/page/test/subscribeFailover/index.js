48e959140f9034f997122e288c02b92d12c18206
bustardcelly <bustardcelly@gmail.com>
2016-10-18 09:34:30 -0400

    typos and bugs in subscriber examples.

--- src/page/test/subscribeFailover/index.js
+++ src/page/test/subscribeFailover/index.js
@@ -17,7 +17,7 @@
   var targetSubscriber;
   var targetView;
 
-  var updateStatusFromEvent = window.red5proHandleSubcriberEvent;
+  var updateStatusFromEvent = window.red5proHandleSubscriberEvent;
   var instanceId = Math.floor(Math.random() * 0x10000).toString(16);
   var streamTitle = document.getElementById('stream-title');
 
