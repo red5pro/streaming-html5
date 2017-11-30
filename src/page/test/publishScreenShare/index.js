aac39618e6b2102ad35f58787949069964beab63
bustardcelly <bustardcelly@gmail.com>
2017-11-30 12:55:27 -0500

    adding in audio support for screen share.

--- src/page/test/publishScreenShare/index.js
+++ src/page/test/publishScreenShare/index.js
@@ -29,15 +29,21 @@
 
 
   var targetPublisher;
+  var audioPublisher;
 
   var updateStatusFromEvent = window.red5proHandlePublisherEvent; // defined in src/template/partial/status-field-publisher.hbs
   var streamTitle = document.getElementById('stream-title');
   var statisticsField = document.getElementById('statistics-field');
   var captureButton = document.getElementById('capture-button');
+  var audioButton = document.getElementById('audio-button');
 
   captureButton.addEventListener('click', function() {
     capture(setupPublisher);
   });
+
+  audioButton.addEventListener('click', function() {
+    setupAudio();
+  })
 
   var protocol = serverSettings.protocol;
   var isSecure = protocol == 'https';
@@ -55,9 +61,15 @@
     console.log('[Red5ProPublisher] ' + event.type + '.');
     updateStatusFromEvent(event);
   }
+
+  function onPublisherAudioEvent (event) {
+    console.log('[Red5ProPublisher:AUDIO] ' + event.type + '.');
+  }
+
   function onPublishFail (message) {
     console.error('[Red5ProPublisher] Publish Error :: ' + message);
   }
+
   function onPublishSuccess (publisher) {
     console.log('[Red5ProPublisher] Publish Complete.');
     try {
@@ -80,13 +92,30 @@
         console.error('[Red5ProPublisher] Desktop Capture Error: ' + error);
         return;
       }
-      cb(screen_constraints);
+      navigator.mediaDevices.enumerateDevices()
+        .then(function (devices) { // eslint-disable-line no-unused-vars
+          // Can't send audio along with constraints for desktop.
+          /*
+          var device, i = devices.length;
+          while(--i > -1) {
+            device = devices[i];
+            if (device.kind.toLowerCase() === 'audioinput') {
+              screen_constraints.audio = {
+                optional: [
+                    {deviceId: device.label || 'microphone1'}
+                  ]
+                }
+              break;
+            }
+            }
+          */
+          cb(screen_constraints);
+        });
     });
   }
 
-  function unpublish () {
+  function unpublish (publisher) {
     return new Promise(function (resolve, reject) {
-      var publisher = targetPublisher;
       publisher.unpublish()
         .then(function () {
           onUnpublishSuccess();
@@ -100,6 +129,32 @@
     });
   }
 
+  function setupAudio () {
+    var audioConfig = Object.assign({}, configuration, {
+      mediaElementId: 'red5pro-audio',
+      protocol: getSocketLocationFromProtocol().protocol,
+      port: getSocketLocationFromProtocol().port,
+      streamName: configuration.stream1 + '_audio',
+      mediaConstraints: {
+        audio: true,
+        video: false
+      }
+    });
+    new red5prosdk.RTCPublisher()
+      .init(audioConfig)
+      .then(function (publisherImpl) {
+        audioPublisher = publisherImpl;
+        audioPublisher.on('*', onPublisherAudioEvent);
+        return audioPublisher.publish();
+      })
+      .then(function () {
+      })
+      .catch(function (error) {
+        var jsonError = typeof error === 'string' ? error : JSON.stringify(error, null, 2);
+        console.error('[Red5ProPublisher:AUDIO] :: Error in publishing audio - ' + jsonError);
+      });
+  }
+
   function setupPublisher (constraints) {
 
     var config = Object.assign({},
@@ -110,7 +165,14 @@
                         port: getSocketLocationFromProtocol().port,
                         streamName: config.stream1,
                         onGetUserMedia: function () {
-                          return navigator.mediaDevices.getUserMedia(constraints);
+                          var c = Object.assign({}, constraints, {
+                          });
+                          c.video.optional.push({
+                            maxWidth: 680
+                          }, {
+                            maxHeight: 480
+                          });
+                          return navigator.mediaDevices.getUserMedia(c);
                         }
                     });
 
@@ -138,9 +200,20 @@
       if (targetPublisher) {
         targetPublisher.off('*', onPublisherEvent);
       }
+      if (audioPublisher) {
+        audioPublisher.off('*', onPublisherAudioEvent);
+      }
       targetPublisher = undefined;
+      audioPublisher = undefined;
     }
-    unpublish().then(clearRefs).catch(clearRefs);
+    unpublish(targetPublisher)
+      .then(function () {
+        if (audioPublisher) {
+          return unpublish(audioPublisher);
+        }
+        return true;
+      })
+      .then(clearRefs).catch(clearRefs);
     window.untrackBitrate();
   });
 
