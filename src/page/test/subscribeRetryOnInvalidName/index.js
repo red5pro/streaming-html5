0b3c5be84999bbb3c2af4cea9aebd63611431a5a
bustardcelly <bustardcelly@gmail.com>
2018-05-03 10:17:01 -0400

    adding retry connection example for subscriber.

---
+++ src/page/test/subscribeRetryOnInvalidName/index.js
@@ -0,0 +1,168 @@
+(function(window, document, red5prosdk) {
+  'use strict';
+
+  var serverSettings = (function() {
+    var settings = sessionStorage.getItem('r5proServerSettings');
+    try {
+      return JSON.parse(settings);
+    }
+    catch (e) {
+      console.error('Could not read server settings from sessionstorage: ' + e.message);
+    }
+    return {};
+  })();
+
+  var configuration = (function () {
+    var conf = sessionStorage.getItem('r5proTestBed');
+    try {
+      return JSON.parse(conf);
+    }
+    catch (e) {
+      console.error('Could not read testbed configuration from sessionstorage: ' + e.message);
+    }
+    return {}
+  })();
+  red5prosdk.setLogLevel(configuration.verboseLogging ? red5prosdk.LOG_LEVELS.TRACE : red5prosdk.LOG_LEVELS.WARN);
+
+  var targetSubscriber;
+  var updateStatusFromEvent = function (event) {
+    var subTypes = red5prosdk.SubscriberEventTypes;
+    switch (event.type) {
+        case subTypes.CONNECT_FAILURE:
+        case subTypes.SUBSCRIBE_FAIL:
+          shutdownVideoElement();
+          break;
+    }
+    window.red5proHandleSubscriberEvent(event); // defined in src/template/partial/status-field-subscriber.hbs
+  };
+  var streamTitle = document.getElementById('stream-title');
+  var retryButton = document.getElementById('retry-button');
+  var statusField = document.getElementById('status-field');
+  var protocol = serverSettings.protocol;
+  var isSecure = protocol === 'https';
+  function getSocketLocationFromProtocol () {
+    return !isSecure
+      ? {protocol: 'ws', port: serverSettings.wsport}
+      : {protocol: 'wss', port: serverSettings.wssport};
+  }
+
+  var defaultConfiguration = (function(useVideo, useAudio) {
+    var c = {
+      protocol: getSocketLocationFromProtocol().protocol,
+      port: getSocketLocationFromProtocol().port,
+      maintainConnectionOnInitErrors: true
+    };
+    if (!useVideo) {
+      c.videoEncoding = red5prosdk.PlaybackVideoEncoder.NONE;
+    }
+    if (!useAudio) {
+      c.audioEncoding = red5prosdk.PlaybackAudioEncoder.NONE;
+    }
+    return c;
+  })(configuration.useVideo, configuration.useAudio);
+
+  function enableRetryButton () {
+    retryButton.removeAttribute('disabled');
+  }
+  function disableRetryButton () {
+    retryButton.setAttribute('disabled', 'disabled');
+  }
+  retryButton.addEventListener('click', function () {
+    statusField.innerText = 'Retrying subscription...'
+    if (targetSubscriber) {
+      disableRetryButton();
+      targetSubscriber.subscribe()
+        .then(function () {
+          onSubscribeSuccess();
+        })
+        .catch(function (error) {
+          var jsonError = typeof error === 'string' ? error : JSON.stringify(error, null, 2);
+          console.error('[Red5ProSubscriber] :: Error in subscribing - ' + jsonError);
+          onSubscribeFail(jsonError);
+        });
+    }
+  });
+
+  function shutdownVideoElement () {
+    var videoElement = document.getElementById('red5pro-subscriber');
+    if (videoElement) {
+      videoElement.pause()
+      videoElement.src = ''
+    }
+  }
+
+  // Local lifecycle notifications.
+  function onSubscriberEvent (event) {
+    console.log('[Red5ProSubsriber] ' + event.type + '.');
+    updateStatusFromEvent(event);
+  }
+  function onSubscribeFail (message) {
+    console.error('[Red5ProSubsriber] Subscribe Error :: ' + message);
+  }
+  function onSubscribeSuccess () {
+    console.log('[Red5ProSubsriber] Subscribe Complete.');
+  }
+  function onUnsubscribeFail (message) {
+    console.error('[Red5ProSubsriber] Unsubscribe Error :: ' + message);
+  }
+  function onUnsubscribeSuccess () {
+    console.log('[Red5ProSubsriber] Unsubscribe Complete.');
+  }
+
+  // Request to unsubscribe.
+  function unsubscribe () {
+    return new Promise(function(resolve, reject) {
+      targetSubscriber.unsubscribe()
+        .then(function () {
+          targetSubscriber.off('*', onSubscriberEvent);
+          targetSubscriber = undefined;
+          onUnsubscribeSuccess();
+          resolve();
+        })
+        .catch(function (error) {
+          var jsonError = typeof error === 'string' ? error : JSON.stringify(error, null, 2);
+          onUnsubscribeFail(jsonError);
+          reject(error);
+        });
+    });
+  }
+
+  // Kick off.
+  var config = Object.assign({}, configuration, defaultConfiguration);
+  config.streamName = config.stream1;
+  var subscriber = new red5prosdk.RTCSubscriber();
+  subscriber.on(window.red5prosdk.SubscriberEventTypes.SUBSCRIBE_INVALID_NAME, function () {
+    enableRetryButton();
+  });
+  disableRetryButton();
+
+  subscriber.init(config)
+    .then(function (subscriberImpl) {
+      streamTitle.innerText = configuration.stream1
+      targetSubscriber = subscriberImpl
+      // Subscribe to events.
+      targetSubscriber.on('*', onSubscriberEvent);
+      return targetSubscriber.subscribe();
+    })
+    .then(function () {
+      onSubscribeSuccess();
+    })
+    .catch(function (error) {
+      var jsonError = typeof error === 'string' ? error : JSON.stringify(error, null, 2);
+      console.error('[Red5ProSubscriber] :: Error in subscribing - ' + jsonError);
+      onSubscribeFail(jsonError);
+    });
+
+  // Clean up.
+  window.addEventListener('beforeunload', function() {
+    function clearRefs () {
+      if (targetSubscriber) {
+        targetSubscriber.off('*', onSubscriberEvent);
+      }
+      targetSubscriber = undefined;
+    }
+    unsubscribe().then(clearRefs).catch(clearRefs);
+  });
+
+})(this, document, window.red5prosdk);
+
