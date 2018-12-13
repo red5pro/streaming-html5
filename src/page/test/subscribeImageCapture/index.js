440127ca07541a09e33e24cdadfc89e5de848502
bustardcelly <bustardcelly@gmail.com>
2018-12-13 13:33:09 +0100

    using video element size for image capture.

--- src/page/test/subscribeImageCapture/index.js
+++ src/page/test/subscribeImageCapture/index.js
@@ -152,14 +152,14 @@
   function clearCanvas (targetElement, canvasElement) {
     var context = canvasElement.getContext('2d');
     context.fillStyle = '#a1a1a1';
-    context.fillRect(0, 0, targetElement.offsetWidth, targetElement.offsetHeight);
+    context.fillRect(0, 0, targetElement.videoWidth, targetElement.videoHeight);
   }
 
   function drawOnCanvas (targetElement, canvasElement) {
     var context = canvasElement.getContext('2d');
-    canvasElement.width = targetElement.offsetWidth;
-    canvasElement.height = targetElement.offsetHeight;
-    context.drawImage(targetElement, 0, 0, targetElement.offsetWidth, targetElement.offsetHeight);
+    canvasElement.width = targetElement.videoWidth;
+    canvasElement.height = targetElement.videoHeight;
+    context.drawImage(targetElement, 0, 0, targetElement.videoWidth, targetElement.videoHeight);
   }
 
   // Kick off.
