176d906fe405002b381292c8a4352c6b80c47f1f
bustardcelly <bustardcelly@gmail.com>
2020-01-16 11:56:10 -0500

    removing streamType for READMEs and a few tests. no longer valid prop.

--- src/page/test/publishHQAudio/index.js
+++ src/page/test/publishHQAudio/index.js
@@ -185,8 +185,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
   var rtcConfig = Object.assign({}, config, {
                       protocol: getSocketLocationFromProtocol().protocol,
                       port: getSocketLocationFromProtocol().port,
-                      streamName: config.stream1,
-                      streamType: 'webrtc'
+                      streamName: config.stream1
                    });
 
   var rtmpConfig = Object.assign({}, config, {
