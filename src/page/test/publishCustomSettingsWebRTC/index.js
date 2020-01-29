0bde732b44b8d4d6087106ab3af418105dffdeea
bustardcelly <bustardcelly@gmail.com>
2020-01-29 10:35:45 -0500

    better camera management on custom settings publisher test.

--- src/page/test/publishCustomSettingsWebRTC/index.js
+++ src/page/test/publishCustomSettingsWebRTC/index.js
@@ -263,34 +263,41 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
     cameraSelect.innerHTML = options.join(' ');
   }
 
-  function establishInitialStream () {
-    var stream;
-    var constraints = getUserMediaConfiguration();
+  function clearEstablishedStream () {
     var pubElement = document.getElementById('red5pro-publisher');
     if (pubElement.srcObject) {
       pubElement.srcObject.getTracks().forEach(function (track) {
         track.stop();
       });
+      return true;
     }
-    if (configuration.useVideo) {
-      delete constraints.video.frameRate;
-    }
-    navigator.mediaDevices.getUserMedia(constraints)
-      .then(function (mediastream) {
-        stream = mediastream;
-        return navigator.mediaDevices.enumerateDevices()
-      })
-      .then(function (devices) {
-        listDevices(devices);
-        stream.getVideoTracks().forEach(function (track) {
-          cameraSelect.value = track.getSettings().deviceId;
+    return false;
+  }
+
+  function establishInitialStream (deviceId) {
+    var stream;
+    var constraints = {audio:false, video: (deviceId ? { deviceId: {exact: deviceId} } : true) };
+    var pubElement = document.getElementById('red5pro-publisher');
+    var delay = clearEstablishedStream() ? 200 : 0;
+    var t = setTimeout(function () {
+      clearTimeout(t);
+      navigator.mediaDevices.getUserMedia(constraints)
+        .then(function (mediastream) {
+          stream = mediastream;
+          return navigator.mediaDevices.enumerateDevices()
+        })
+        .then(function (devices) {
+          listDevices(devices);
+          stream.getVideoTracks().forEach(function (track) {
+            cameraSelect.value = track.getSettings().deviceId;
+          });
+          pubElement.srcObject = stream;
+        })
+        .catch(function (error) {
+          console.error(error);
+          showConstraintError(error.message, error.constraint || 'N/A');
         });
-        pubElement.srcObject = stream;
-      })
-      .catch(function (error) {
-        console.error(error);
-        showConstraintError(error.message, error.constraint || 'N/A');
-      });
+    }, delay);
   }
 
   function onCameraSelect () {
@@ -387,27 +394,32 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
   function startStopPublish () {
     if (isPublishable) {
       setPublishableState(false);
-      determinePublisher()
-        .then(function (publisherImpl) {
-          streamTitle.innerText = configuration.stream1;
-          targetPublisher = publisherImpl;
-          return targetPublisher.publish()
-        })
-        .then(function () {
-          onPublishSuccess(targetPublisher);
-        })
-        .catch(function (error) {
-          var jsonError = typeof error === 'string' ? error : JSON.stringify(error, null, 2);
-          console.error('[Red5ProPublisher] :: Error in publishing - ' + jsonError);
-          onPublishFail(jsonError);
-        });
+      // delay by 200 milliseconds to allow for clearage of previous stream.
+      var delay = clearEstablishedStream() ? 200 : 0;
+      var t = setTimeout(function (){
+        clearTimeout(t);
+        determinePublisher()
+          .then(function (publisherImpl) {
+            streamTitle.innerText = configuration.stream1;
+            targetPublisher = publisherImpl;
+            return targetPublisher.publish()
+          })
+          .then(function () {
+            onPublishSuccess(targetPublisher);
+          })
+          .catch(function (error) {
+            var jsonError = typeof error === 'string' ? error : JSON.stringify(error, null, 2);
+            console.error('[Red5ProPublisher] :: Error in publishing - ' + jsonError);
+            onPublishFail(jsonError);
+          });
+      }, delay);
     } else {
       shutdown(false);
     }
   }
 
   function restart() {
-    establishInitialStream();
+    establishInitialStream((cameraSelect.value && cameraSelect.value.length > 0) ? cameraSelect.value : undefined);
   }
 
   navigator.mediaDevices.enumerateDevices()
