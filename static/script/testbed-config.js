49d048802d7c6e0db6dd1f14702b00d40c1b8cd7
bustardcelly <bustardcelly@gmail.com>
2020-06-05 16:42:52 -0400

    SDK upped to 7.1.0-RC.

--- static/script/testbed-config.js
+++ static/script/testbed-config.js
@@ -67,7 +67,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
     json = {
       "version": build_version,
       "host": window.location.hostname,
-      "port": 8554, // rtsp
+      "port": 5080,
       "stream1": "stream1",
       "stream2": "stream2",
       "app": "live",
