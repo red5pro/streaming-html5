e310cea2227d7dc4c74d0ebcc16d19da31c3e22e
Todd Anderson <bustardcelly@gmail.com>
2021-09-29 15:40:43 -0400

    adding auth to access SM VOD Meta. Fixes NVID-35

--- src/page/sm-test/subscribeStreamManagerProxyAMFMetadataVOD/index.js
+++ src/page/sm-test/subscribeStreamManagerProxyAMFMetadataVOD/index.js
@@ -301,6 +301,9 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
     var baseUrl = protocol + '://' + host + ':' + port;
     var apiVersion = configuration.streamManagerAPI || '4.0';
     var url = baseUrl + '/streammanager/api/' + apiVersion + '/media/' + app + '/playlists';
+    if (configuration.authentication.enabled) {
+      url += `?${getAuthQueryParams()}`
+    }
     return new Promise(function (resolve, reject) {
       fetch(url)
         .then(function (res) {
