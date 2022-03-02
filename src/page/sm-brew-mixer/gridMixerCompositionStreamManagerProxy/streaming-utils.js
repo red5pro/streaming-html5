442578ab825c8dcbeb1c964ff09e18bdcfb471b6
david179 <31712615+david179@users.noreply.github.com>
2022-03-02 16:36:09 -0500

    add utils functions to subscribe to streams

---
+++ src/page/sm-brew-mixer/gridMixerCompositionStreamManagerProxy/streaming-utils.js
@@ -0,0 +1,337 @@
+/*
+Copyright © 2015 Infrared5, Inc. All rights reserved.
+
+The accompanying code comprising examples for use solely in conjunction with Red5 Pro (the "Example Code") 
+is  licensed  to  you  by  Infrared5  Inc.  in  consideration  of  your  agreement  to  the  following  
+license terms  and  conditions.  Access,  use,  modification,  or  redistribution  of  the  accompanying  
+code  constitutes your acceptance of the following license terms and conditions.
+
+Permission is hereby granted, free of charge, to you to use the Example Code and associated documentation 
+files (collectively, the "Software") without restriction, including without limitation the rights to use, 
+copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit 
+persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The Software shall be used solely in conjunction with Red5 Pro. Red5 Pro is licensed under a separate end 
+user  license  agreement  (the  "EULA"),  which  must  be  executed  with  Infrared5,  Inc.   
+An  example  of  the EULA can be found on our website at: https://account.red5pro.com/assets/LICENSE.txt.
+
+The above copyright notice and this license shall be included in all copies or portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,  INCLUDING  BUT  
+NOT  LIMITED  TO  THE  WARRANTIES  OF  MERCHANTABILITY, FITNESS  FOR  A  PARTICULAR  PURPOSE  AND  
+NONINFRINGEMENT.   IN  NO  EVENT  SHALL INFRARED5, INC. BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN  AN  ACTION  OF  CONTRACT,  TORT  OR  OTHERWISE,  ARISING  FROM,  OUT  OF  OR  IN CONNECTION 
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+((window, red5prosdk) => {
+
+
+    red5prosdk.setLogLevel('debug')
+
+    let serverSettings = (function () {
+        let settings = sessionStorage.getItem('r5proServerSettings')
+        try {
+            return JSON.parse(settings)
+        }
+        catch (e) {
+            console.error('Could not read server settings from sessionstorage: ' + e.message)
+        }
+        return {}
+    })()
+
+    let configuration = (function () {
+        let conf = sessionStorage.getItem('r5proTestBed')
+        try {
+            return JSON.parse(conf)
+        }
+        catch (e) {
+            console.error('Could not read testbed configuration from sessionstorage: ' + e.message)
+        }
+        return {}
+    })()
+
+    let protocol = serverSettings.protocol
+    let isSecure = protocol === 'https'
+    function getSocketLocationFromProtocol() {
+        return !isSecure
+            ? { protocol: 'ws', port: serverSettings.wsport }
+            : { protocol: 'wss', port: serverSettings.wssport }
+    }
+
+    let defaultConfiguration = {
+        protocol: getSocketLocationFromProtocol().protocol,
+        port: getSocketLocationFromProtocol().port,
+        streamMode: configuration.recordBroadcast ? 'record' : 'live'
+    }
+
+    let targetSubscriber
+    let instanceId = Math.floor(Math.random() * 0x10000).toString(16)
+    let streamTitle = document.getElementById('stream-title')
+    let statisticsField = document.getElementById('statistics-field')
+    let bitrateField = document.getElementById('bitrate-field')
+    let packetsField = document.getElementById('packets-field')
+    let resolutionField = document.getElementById('resolution-field')
+    let addressField = document.getElementById('address-field')
+    let path = null
+    let streamName = null
+
+    let bitrate = 0
+    let packetsReceived = 0
+    let frameWidth = 0
+    let frameHeight = 0
+    function updateStatistics(b, p, w, h) {
+        statisticsField.classList.remove('hidden')
+        bitrateField.innerText = b === 0 ? 'N/A' : Math.floor(b)
+        packetsField.innerText = p
+        resolutionField.innerText = (w || 0) + 'x' + (h || 0)
+    }
+
+    function onBitrateUpdate(b, p) {
+        bitrate = b
+        packetsReceived = p
+        updateStatistics(bitrate, packetsReceived, frameWidth, frameHeight)
+    }
+
+    function onResolutionUpdate(w, h) {
+        frameWidth = w
+        frameHeight = h
+        updateStatistics(bitrate, packetsReceived, frameWidth, frameHeight)
+    }
+
+    let updateStatusFromEvent = function (event) {
+        let subTypes = red5prosdk.SubscriberEventTypes
+        switch (event.type) {
+            case subTypes.CONNECT_FAILURE:
+            case subTypes.SUBSCRIBE_FAIL:
+                shutdownVideoElement()
+                break
+        }
+        window.red5proHandleSubscriberEvent(event)  // defined in src/template/partial/status-field-subscriber.hbs
+    }
+
+    function shutdownVideoElement() {
+        let videoElement = document.getElementById('red5pro-subscriber')
+        if (videoElement) {
+            videoElement.pause()
+            videoElement.src = ''
+        }
+    }
+
+    function getAuthenticationParams() {
+        let auth = configuration.authentication
+        return auth && auth.enabled
+            ? {
+                connectionParams: {
+                    username: auth.username,
+                    password: auth.password
+                }
+            }
+            : {}
+    }
+
+    function displayServerAddress(serverAddress, type) {
+        addressField.innerText = type + ' Address: ' + serverAddress
+    }
+
+
+    // Local lifecycle notifications.
+    function onSubscriberEvent(event) {
+        console.log('[Red5ProSubsriber] ' + event.type + '.')
+        updateStatusFromEvent(event)
+        if (event.type === 'Subscribe.VideoDimensions.Change') {
+            onResolutionUpdate(event.data.width, event.data.height)
+        }
+    }
+    function onSubscribeFail(message) {
+        console.error('[Red5ProSubsriber] Subscribe Error :: ' + message)
+    }
+    function onSubscribeSuccess(subscriber) {
+        console.log('[Red5ProSubsriber] Subscribe Complete.')
+        if (window.exposeSubscriberGlobally) {
+            window.exposeSubscriberGlobally(subscriber)
+        }
+        if (subscriber.getType().toLowerCase() === 'rtc') {
+            try {
+                window.trackBitrate(subscriber.getPeerConnection(), onBitrateUpdate, onResolutionUpdate, true)
+            }
+            catch (e) {
+                //
+            }
+        }
+    }
+    function onUnsubscribeFail(message) {
+        console.error('[Red5ProSubsriber] Unsubscribe Error :: ' + message)
+    }
+    function onUnsubscribeSuccess() {
+        console.log('[Red5ProSubsriber] Unsubscribe Complete.')
+    }
+
+
+    function requestEdge(configuration) {
+        let host = configuration.host
+        let app = path
+        let port = serverSettings.httpport
+        let baseUrl = protocol + '://' + host + ':' + port
+        let streamName = this.streamName
+        let apiVersion = configuration.streamManagerAPI || '4.0'
+        let url = baseUrl + '/streammanager/api/' + apiVersion + '/event/' + app + '/' + streamName + '?action=subscribe'
+        return new Promise(function (resolve, reject) {
+            fetch(url)
+                .then(function (res) {
+                    if (res.headers.get("content-type") &&
+                        res.headers.get("content-type").toLowerCase().indexOf("application/json") >= 0) {
+                        return res.json()
+                    }
+                    else {
+                        throw new TypeError('Could not properly parse response.')
+                    }
+                })
+                .then(function (json) {
+                    resolve(json)
+                })
+                .catch(function (error) {
+                    let jsonError = typeof error === 'string' ? error : JSON.stringify(error, null, 2)
+                    console.error('[SubscribeStreamManagerTest] :: Error - Could not request Edge IP from Stream Manager. ' + jsonError)
+                    reject(error)
+                })
+        })
+    }
+
+    function determineSubscriber(jsonResponse) {
+        let host = jsonResponse.serverAddress
+        let name = jsonResponse.name
+        let app = jsonResponse.scope
+        displayServerAddress('Edge', host)
+        let config = Object.assign({},
+            configuration,
+            defaultConfiguration,
+            getAuthenticationParams())
+        let rtcConfig = Object.assign({}, config, {
+            host: host,
+            protocol: 'ws', // cluster is not over secure, at this time
+            port: serverSettings.wsport, // cluster is not over secure, at this time
+            subscriptionId: 'subscriber-' + instanceId,
+            streamName: this.streamName
+        })
+        let rtmpConfig = Object.assign({}, config, {
+            host: host,
+            app: app,
+            protocol: 'rtmp',
+            port: serverSettings.rtmpport,
+            streamName: this.streamName,
+            width: config.cameraWidth,
+            height: config.cameraHeight,
+            swf: '../../lib/red5pro/red5pro-subscriber.swf',
+            swfobjectURL: '../../lib/swfobject/swfobject.js',
+            productInstallURL: '../../lib/swfobject/playerProductInstall.swf'
+        })
+        let hlsConfig = Object.assign({}, config, {
+            host: host,
+            protocol: 'http',
+            app: app,
+            port: serverSettings.hlsport,
+            streamName: name,
+            mimeType: 'application/x-mpegURL'
+        })
+
+        let subscribeOrder = config.subscriberFailoverOrder
+            .split(',').map(function (item) {
+                return item.trim()
+            })
+
+        if (window.query('view')) {
+            subscribeOrder = [window.query('view')]
+        }
+
+        let subscriber = new red5prosdk.Red5ProSubscriber()
+        return subscriber.setPlaybackOrder(subscribeOrder).init({
+            rtc: rtcConfig,
+            rtmp: rtmpConfig,
+            hls: hlsConfig
+        })
+    }
+
+    // Request to unsubscribe.
+    function unsubscribe() {
+        return new Promise(function (resolve, reject) {
+            let subscriber = targetSubscriber
+            subscriber.unsubscribe()
+                .then(function () {
+                    targetSubscriber.off('*', onSubscriberEvent)
+                    targetSubscriber = undefined
+                    onUnsubscribeSuccess()
+                    resolve()
+                })
+                .catch(function (error) {
+                    let jsonError = typeof error === 'string' ? error : JSON.stringify(error, null, 2)
+                    onUnsubscribeFail(jsonError)
+                    reject(error)
+                })
+        })
+    }
+
+    let retryCount = 0
+    let retryLimit = 10
+    function respondToEdge(response) {
+        determineSubscriber(response)
+            .then(function (subscriberImpl) {
+                streamTitle.innerText = configuration.stream1
+                targetSubscriber = subscriberImpl
+                // Subscribe to events.
+                targetSubscriber.on('*', onSubscriberEvent)
+                return targetSubscriber.subscribe()
+            })
+            .then(function (sub) {
+                onSubscribeSuccess(sub)
+            })
+            .catch(function (error) {
+                let jsonError = typeof error === 'string' ? error : JSON.stringify(error, null, 2)
+                console.error('[Red5ProSubscriber] :: Error in subscribing - ' + jsonError)
+                onSubscribeFail(jsonError)
+            })
+    }
+
+    function respondToEdgeFailure(error) {
+        if (retryCount++ < retryLimit) {
+            let retryTimer = setTimeout(function () {
+                clearTimeout(retryTimer)
+                doSsubscribeToMixedStream()
+            }, 1000)
+        }
+        else {
+            let jsonError = typeof error === 'string' ? error : JSON.stringify(error, null, 2)
+            console.error('[Red5ProSubscriber] :: Retry timeout in subscribing - ' + jsonError)
+        }
+    }
+
+    function subscribeToMixedStream(path, streamName) {
+        this.path = path
+        this.streamName = streamName
+        doSsubscribeToMixedStream()
+    }
+
+    function doSsubscribeToMixedStream() {
+        requestEdge(configuration)
+            .then(respondToEdge)
+            .catch(respondToEdgeFailure)
+    }
+
+    let shuttingDown = false
+    function shutdown() {
+        if (shuttingDown) return
+        shuttingDown = true
+        function clearRefs() {
+            if (targetSubscriber) {
+                targetSubscriber.off('*', onSubscriberEvent)
+            }
+            targetSubscriber = undefined
+        }
+        unsubscribe().then(clearRefs).catch(clearRefs)
+        window.untrackBitrate()
+    }
+    window.addEventListener('pagehide', shutdown)
+    window.addEventListener('beforeunload', shutdown)
+
+    window.subscribeToMixedStream = subscribeToMixedStream
+})(window,
+    window.red5prosdk)
