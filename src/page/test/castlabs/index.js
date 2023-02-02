8fc07cda1f77a2fb561329f565f122d054dcd9e9
Todd Anderson <bustardcelly@gmail.com>
2023-02-02 15:57:28 -0500

    some layout and UI for castlabs test.

--- src/page/test/castlabs/index.js
+++ src/page/test/castlabs/index.js
@@ -1,4 +1,29 @@
-import { setDRM, videoTransformFunction, audioTransformFunction, Environments } from '../../lib/castlabs/rtc-drm-transform/rtc-drm-transform.min.js'
+/*
+Copyright © 2015 Infrared5, Inc. All rights reserved.
+
+The accompanying code comprising examples for use solely in conjunction with Red5 Pro (the "Example Code") 
+is  licensed  to  you  by  Infrared5  Inc.  in  consideration  of  your  agreement  to  the  following  
+license terms  and  conditions.  Access,  use,  modification,  or  redistribution  of  the  accompanying  
+code  constitutes your acceptance of the following license terms and conditions.
+
+Permission is hereby granted, free of charge, to you to use the Example Code and associated documentation 
+files (collectively, the "Software") without restriction, including without limitation the rights to use, 
+copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit 
+persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The Software shall be used solely in conjunction with Red5 Pro. Red5 Pro is licensed under a separate end 
+user  license  agreement  (the  "EULA"),  which  must  be  executed  with  Infrared5,  Inc.   
+An  example  of  the EULA can be found on our website at: https://account.red5pro.com/assets/LICENSE.txt.
+
+The above copyright notice and this license shall be included in all copies or portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,  INCLUDING  BUT  
+NOT  LIMITED  TO  THE  WARRANTIES  OF  MERCHANTABILITY, FITNESS  FOR  A  PARTICULAR  PURPOSE  AND  
+NONINFRINGEMENT.   IN  NO  EVENT  SHALL INFRARED5, INC. BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN  AN  ACTION  OF  CONTRACT,  TORT  OR  OTHERWISE,  ARISING  FROM,  OUT  OF  OR  IN CONNECTION 
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+import { setDrm, videoTransformFunction, audioTransformFunction, Environments } from '../../lib/castlabs/rtc-drm-transform/rtc-drm-transform.min.js'
 
 var serverSettings = (function() {
   var settings = sessionStorage.getItem('r5proServerSettings');
@@ -29,7 +54,20 @@ let encryption = 'cbcs'
 let iv = encryption !== 'cbcs' ? null : new Uint8Array([0xd5, 0xfb, 0xd6, 0xb8, 0x2e, 0xd9, 0x3e, 0x4e, 0xf9, 0x8a, 0xe4, 0x09, 0x31, 0xee, 0x33, 0xb7])
 let keyId = encryption === 'cbcs' ? null : new Uint8Array([0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35])
 
+const getPortAndProtocol = host => {
+  let ipReg = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/
+  let localhostReg = /^localhost.*/
+  let isIPOrLocalhost = ipReg.exec(host) || localhostReg.exec(host)
+  let port = isIPOrLocalhost ? 5080 : 443
+  let protocol = isIPOrLocalhost ? 'ws' : 'wss'
+  return { port, protocol }
+}
+
+const { port, protocol } = getPortAndProtocol(configuration.host)
 const baseConfig = {...configuration,
+  protocol,
+  port,
+  streamName: configuration.stream1,
   rtcConfiguration: {
       iceServers: [{urls: 'stun:stun2.l.google.com:19302'}],
       iceCandidatePoolSize: 2,
@@ -61,15 +99,18 @@ const onSubscriberEvent = event => {
     console.log(type)
 }
 
-const encryptPlayback = async () => {
+const encryptedPlayback = async () => {
   try {
     const { rtcConfiguration } = baseConfig
-    encryptedSubscriber = await new red5prosdk.RTCSubscriber().init({...baseConfig, rtcConfiguration: {
-      ...rtcConfiguration,
+    encryptedSubscriber = await new red5prosdk.RTCSubscriber().init({...baseConfig,
       mediaElementId: 'red5pro-encrypted',
-      encodedInsertableStreams: false
-    }})
-    await subscriber.subscribe()
+      rtcConfiguration: {
+        ...rtcConfiguration,
+        encodedInsertableStreams: false
+      }
+    })
+    window.encryptedSubscriber = encryptedSubscriber
+    await encryptedSubscriber.subscribe()
   } catch (e) {
     console.error(e)
   }
@@ -106,3 +147,5 @@ const decryptPlayback = async () => {
       console.error(e)
   }
 }
+
+encryptedPlayback()
