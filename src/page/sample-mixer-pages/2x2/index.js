04d073035b6023cef5aa06980f23777dff2b6f00
Todd Anderson <bustardcelly@gmail.com>
2023-04-11 11:53:06 -0400

    more mixer update for whipwhep.

--- src/page/sample-mixer-pages/2x2/index.js
+++ src/page/sample-mixer-pages/2x2/index.js
@@ -1,51 +1,52 @@
 /*
 Copyright © 2015 Infrared5, Inc. All rights reserved.
 
-The accompanying code comprising examples for use solely in conjunction with Red5 Pro (the "Example Code") 
-is  licensed  to  you  by  Infrared5  Inc.  in  consideration  of  your  agreement  to  the  following  
-license terms  and  conditions.  Access,  use,  modification,  or  redistribution  of  the  accompanying  
+The accompanying code comprising examples for use solely in conjunction with Red5 Pro (the "Example Code")
+is  licensed  to  you  by  Infrared5  Inc.  in  consideration  of  your  agreement  to  the  following
+license terms  and  conditions.  Access,  use,  modification,  or  redistribution  of  the  accompanying
 code  constitutes your acceptance of the following license terms and conditions.
 
-Permission is hereby granted, free of charge, to you to use the Example Code and associated documentation 
-files (collectively, the "Software") without restriction, including without limitation the rights to use, 
-copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit 
+Permission is hereby granted, free of charge, to you to use the Example Code and associated documentation
+files (collectively, the "Software") without restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
 persons to whom the Software is furnished to do so, subject to the following conditions:
 
-The Software shall be used solely in conjunction with Red5 Pro. Red5 Pro is licensed under a separate end 
-user  license  agreement  (the  "EULA"),  which  must  be  executed  with  Infrared5,  Inc.   
+The Software shall be used solely in conjunction with Red5 Pro. Red5 Pro is licensed under a separate end
+user  license  agreement  (the  "EULA"),  which  must  be  executed  with  Infrared5,  Inc.
 An  example  of  the EULA can be found on our website at: https://account.red5pro.com/assets/LICENSE.txt.
 
 The above copyright notice and this license shall be included in all copies or portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,  INCLUDING  BUT  
-NOT  LIMITED  TO  THE  WARRANTIES  OF  MERCHANTABILITY, FITNESS  FOR  A  PARTICULAR  PURPOSE  AND  
-NONINFRINGEMENT.   IN  NO  EVENT  SHALL INFRARED5, INC. BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
-WHETHER IN  AN  ACTION  OF  CONTRACT,  TORT  OR  OTHERWISE,  ARISING  FROM,  OUT  OF  OR  IN CONNECTION 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,  INCLUDING  BUT
+NOT  LIMITED  TO  THE  WARRANTIES  OF  MERCHANTABILITY, FITNESS  FOR  A  PARTICULAR  PURPOSE  AND
+NONINFRINGEMENT.   IN  NO  EVENT  SHALL INFRARED5, INC. BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN  AN  ACTION  OF  CONTRACT,  TORT  OR  OTHERWISE,  ARISING  FROM,  OUT  OF  OR  IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-((window, red5prosdk, SubscriberBlock) => {
-
+;((window, red5prosdk, SubscriberBlock) => {
   var serverSettings = (function () {
-    var settings = sessionStorage.getItem('r5proServerSettings');
+    var settings = sessionStorage.getItem('r5proServerSettings')
     try {
-      return JSON.parse(settings);
-    }
-    catch (e) {
-      console.error('Could not read server settings from sessionstorage: ' + e.message);
-    }
-    return {};
-  })();
-
-  var configuration = (function () {
-    var conf = sessionStorage.getItem('r5proTestBed');
-    try {
-      return JSON.parse(conf);
-    }
-    catch (e) {
-      console.error('Could not read testbed configuration from sessionstorage: ' + e.message);
+      return JSON.parse(settings)
+    } catch (e) {
+      console.error(
+        'Could not read server settings from sessionstorage: ' + e.message
+      )
     }
     return {}
-  })();
+  })()
+
+  var configuration = (function () {
+    var conf = sessionStorage.getItem('r5proTestBed')
+    try {
+      return JSON.parse(conf)
+    } catch (e) {
+      console.error(
+        'Could not read testbed configuration from sessionstorage: ' + e.message
+      )
+    }
+    return {}
+  })()
 
   //  red5prosdk.setLogLevel('debug')
 
@@ -58,16 +59,19 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
   const scope = roomName === '' ? appContext : `${appContext}/${roomName}`
   const sm = window.query('sm') || 'true'
   const requiresStreamManager = !sm ? false : !(sm && sm === 'false')
+  const whipwhep = window.query('whipwhep') || 'true'
+  const preferWhipWhep = !whipwhep ? false : !(whipwhep && whipwhep === 'false')
   const ws = window.query('ws') || 'null'
   const webSocketEndpointForLayouts = `wss://${ws}?testbed=grid&type=cef&id=${cefId}&event-id=${eventId}`
-  const red5ProHost = window.query('host') || 'localhost'//configuration.host
+  const red5ProHost = window.query('host') || 'localhost' //configuration.host
   const streamManagerHost = configuration.host
 
   // Round Trip Authentication
   const username = window.query('username') || 'default-username'
   const password = window.query('password') || 'default-password'
   const token = JSON.stringify({
-    token: window.query('token') || 'default-token', room: roomName
+    token: window.query('token') || 'default-token',
+    room: roomName,
   })
 
   //const layoutName = window.query('layoutname') || 'squaresmix'
@@ -76,32 +80,38 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
   let ipReg = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/
   let localhostReg = /^localhost.*/
-  let isIPOrLocalhost = ipReg.exec(red5ProHost) || localhostReg.exec(red5ProHost)
+  let isIPOrLocalhost =
+    ipReg.exec(red5ProHost) || localhostReg.exec(red5ProHost)
   let secureConnection = !isIPOrLocalhost
   let activeSubscribers = {}
 
   let setStreamsToSubscribeTo = new Set()
 
-
   function getUserMediaConfiguration() {
     return {
       mediaConstraints: {
-        audio: configuration.useAudio ? configuration.mediaConstraints.audio : false,
-        video: configuration.useVideo ? configuration.mediaConstraints.video : false
-      }
-    };
+        audio: configuration.useAudio
+          ? configuration.mediaConstraints.audio
+          : false,
+        video: configuration.useVideo
+          ? configuration.mediaConstraints.video
+          : false,
+      },
+    }
   }
 
   const defaultConfiguration = {
     protocol: 'ws',
     port: '5080',
-    streamMode: configuration.recordBroadcast ? 'record' : 'live'
+    streamMode: configuration.recordBroadcast ? 'record' : 'live',
   }
 
-  var config = Object.assign({},
+  var config = Object.assign(
+    {},
     configuration,
     defaultConfiguration,
-    getUserMediaConfiguration())
+    getUserMediaConfiguration()
+  )
 
   // Red5 Pro configuration to subscribe
   var baseConfig = Object.assign({}, config, {
@@ -115,10 +125,9 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
       app: scope,
       username,
       password,
-      token
-    }
+      token,
+    },
   })
-
 
   // UI Configurations
   const utilizeSubscriberNotifications = false
@@ -136,8 +145,11 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
       if (children && children.length === 0) {
         //return { slot, index: i }
         return slot
-      }
-      else if (children && children.length === 1 && children.item(0).className === 'slot-image') {
+      } else if (
+        children &&
+        children.length === 1 &&
+        children.item(0).className === 'slot-image'
+      ) {
         //return { slot, index: i }
         return slot
       }
@@ -145,16 +157,16 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
   }
 
   const onSubscriberEvent = (event, subscriber) => {
-    const {
-      type
-    } = event
+    const { type } = event
     if (type === 'Subscribe.Start') {
       subscriber.getElementContainer().classList.remove('hidden')
-    } else if (type === 'Subscribe.Fail' ||
+    } else if (
+      type === 'Subscribe.Fail' ||
       type === 'Subscribe.InvalidName' ||
       type === 'Subscribe.Stop' ||
       type === 'Subscribe.Play.Unpublish' ||
-      type === 'Subscribe.Connection.Closed') {
+      type === 'Subscribe.Connection.Closed'
+    ) {
       subscriber.getElementContainer().classList.add('hidden')
     }
   }
@@ -168,14 +180,26 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
   const startSubscribers = (streamList) => {
     console.log('sub to ', streamList)
     if (Object.keys(activeSubscribers).length >= rowCount * rowCount) {
-      console.warn('Page is already subscribing to the maximum number of streams. Ignore new subscribe request')
+      console.warn(
+        'Page is already subscribing to the maximum number of streams. Ignore new subscribe request'
+      )
       return
     }
 
-    console.log(`[mixer]:: Starting new subscribers from list: ${JSON.toString(streamList, null, 2)}`)
-    const subscribers = streamList.map(name => {
+    console.log(
+      `[mixer]:: Starting new subscribers from list: ${JSON.toString(
+        streamList,
+        null,
+        2
+      )}`
+    )
+    const subscribers = streamList.map((name) => {
       let freeSlot = findNextAvailableSlot()
-      let bl = new SubscriberBlock(name, freeSlot, utilizeSubscriberNotifications)
+      let bl = new SubscriberBlock(
+        name,
+        freeSlot,
+        utilizeSubscriberNotifications
+      )
       activeSubscribers[name] = bl
       return bl
     })
@@ -186,7 +210,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
         sub.next = subscribers[index + 1]
       }
       if (index === 0) {
-        sub.start(baseConfig, streamManagerHost)
+        sub.start(baseConfig, streamManagerHost, preferWhipWhep)
       }
     })
   }
@@ -210,18 +234,17 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
       slotHeight = slotWidth
     }
     //console.log(slots.length)
-    slots.forEach(slot => {
+    slots.forEach((slot) => {
       slot.style.width = `${slotWidth}px`
       slot.style.height = `${slotHeight}px`
     })
     sectionContainer.style.width = `${slotWidth * rowCount}px`
   }
 
-
-  const webSocket = new WebSocket(webSocketEndpointForLayouts);
+  const webSocket = new WebSocket(webSocketEndpointForLayouts)
 
   webSocket.onmessage = function (event) {
-    console.log(event);
+    console.log(event)
     const data = JSON.parse(event.data)
     processWSMessage(data)
   }
@@ -230,10 +253,12 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
    * WebSocket API
    */
   const processWSMessage = function (message) {
-    if (Object.prototype.hasOwnProperty.call(message, 'type') && message.type === 'compositionUpdate') {
+    if (
+      Object.prototype.hasOwnProperty.call(message, 'type') &&
+      message.type === 'compositionUpdate'
+    ) {
       processCompositionUpdate(message)
-    }
-    else {
+    } else {
       console.log('Unrecognize message received.', message)
     }
   }
@@ -242,20 +267,32 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
    * Processes the composition update messages sent by the Editor page and forwarded by the Node.js mixerServer.
    */
   const processCompositionUpdate = function (message) {
-    if (Object.prototype.hasOwnProperty.call(message, 'add') && message.add.length > 0) {
+    if (
+      Object.prototype.hasOwnProperty.call(message, 'add') &&
+      message.add.length > 0
+    ) {
       const streamsToAdd = getNewStreamsToAdd(message)
       startSubscribers(streamsToAdd)
     }
 
-    if (Object.prototype.hasOwnProperty.call(message, 'remove') && message.remove.length > 0) {
+    if (
+      Object.prototype.hasOwnProperty.call(message, 'remove') &&
+      message.remove.length > 0
+    ) {
       removeStreams(message)
     }
 
-    if (Object.prototype.hasOwnProperty.call(message, 'mute') && message.mute.length > 0) {
+    if (
+      Object.prototype.hasOwnProperty.call(message, 'mute') &&
+      message.mute.length > 0
+    ) {
       muteStreams(message)
     }
 
-    if (Object.prototype.hasOwnProperty.call(message, 'unmute') && message.unmute.length > 0) {
+    if (
+      Object.prototype.hasOwnProperty.call(message, 'unmute') &&
+      message.unmute.length > 0
+    ) {
       unmuteStreams(message)
     }
   }
@@ -268,7 +305,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
     let streamsToAdd = []
     for (let stream of addArray) {
       if (!setStreamsToSubscribeTo.has(stream)) {
-        streamsToAdd.push(stream);
+        streamsToAdd.push(stream)
       }
       setStreamsToSubscribeTo.add(stream)
     }
@@ -290,14 +327,16 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
   }
 
   /**
-     * Mutes the specified streams in the composition.
-     */
+   * Mutes the specified streams in the composition.
+   */
   const muteStreams = function (message) {
     const muteArray = message.mute
     for (let stream of muteArray) {
       if (activeSubscribers[stream]) {
         console.log('mute stream: ', stream)
-        const video = document.getElementById(activeSubscribers[stream].subscriptionId)
+        const video = document.getElementById(
+          activeSubscribers[stream].subscriptionId
+        )
         if (video) {
           video.muted = true
         }
@@ -313,7 +352,9 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
     for (let stream of unmuteArray) {
       if (activeSubscribers[stream]) {
         console.log('unmute stream: ', stream)
-        const video = document.getElementById(activeSubscribers[stream].subscriptionId)
+        const video = document.getElementById(
+          activeSubscribers[stream].subscriptionId
+        )
         if (video) {
           video.muted = false
         }
@@ -321,12 +362,11 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
     }
   }
 
-
   /**
    * Sets placeholder images on the players to show the grid.
    */
-  const setSubscriberImages = images => {
-    const imgs = images.map(url => {
+  const setSubscriberImages = (images) => {
+    const imgs = images.map((url) => {
       const img = document.createElement('img')
       img.src = url
       img.classList.add('slot-image')
@@ -340,12 +380,8 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
   // Main.
   //parseLayout(layoutName)
   resizeSlots()
-  setSubscriberImages(new Array(9).fill('../../css/assets/Red5Pro_logo_white_red.svg'))
+  setSubscriberImages(
+    new Array(9).fill('../../css/assets/Red5Pro_logo_white_red.svg')
+  )
   window.addEventListener('resize', resizeSlots, true)
-
 })(window, window.red5prosdk, window.SubscriberBlock)
-
-
-
-
-
