ab0f3c55f175afa24114164f4adf5e7a5284a8a2
Todd Anderson <bustardcelly@gmail.com>
2024-12-10 16:32:09 +0100

    feat: codec support tests.

---
+++ src/page/test/publishCodecSupport/README.md
@@ -0,0 +1,134 @@
+# Publishing Using Codec Support
+
+This example demonstrates a request for a `MediaStream` with a defined `video` source for the constraint.
+
+**Please refer to the [Basic Publisher Documentation](../publish/README.md) to learn more about the basic setup.**
+
+## Example Code
+
+- **[index.html](index.html)**
+- **[index.js](index.js)**
+
+# Camera Selection
+
+Allow the User to select from a list of cameras detected by their browser. To access the list of camera devices, use the `enumerateDevices` API:
+
+```js
+function listDevices(devices) {
+  var cameras = devices.filter(function (item) {
+    return item.kind === 'videoinput'
+  })
+  var options = cameras.map(function (camera, index) {
+    return (
+      '<option value="' +
+      camera.deviceId +
+      '">' +
+      (camera.label || 'camera ' + index) +
+      '</option>'
+    )
+  })
+  cameraSelect.innerHTML = options.join(' ')
+}
+
+navigator.mediaDevices.enumerateDevices().then(listDevices).catch(onDeviceError)
+```
+
+The camera devices are filtered out from the list returned on `enumerateDevices` based on the `kind` attribute being of `videoinput` value. The list is then provided a a `select` element with an event handler assigned.
+
+Upon a `change` event, the camera `deviceId` is used to assign the `video` constraint for the `MediaStream`.
+
+# Selection & Assignment
+
+Constraints for the audio and video components are defined when accessing a `MediaStream` from the browser.
+
+To define a stream with a specific camera device for publishing, update the `video` constraint used in requesting a `MediaStream` and declare a `sourceId` with the selected camera `deviceId`.
+
+The following `change` event handler is invoked upon User selection of a camera from the `select` element populated in the previous section:
+
+```js
+var mediaConstraints = {
+  audio: configuration.useAudio ? configuration.mediaConstraints.audio : false,
+  video: configuration.useVideo ? configuration.mediaConstraints.video : false,
+}
+
+function onCameraSelect() {
+  if (!configuration.useVideo) {
+    return
+  }
+  var selection = cameraSelect.value
+  if (mediaConstraints.video && typeof mediaConstraints.video !== 'boolean') {
+    mediaConstraints.video.deviceId = { exact: selection }
+    delete mediaConstraints.video.frameRate
+  } else {
+    mediaConstraints.video = {
+      deviceId: { exact: selection },
+    }
+  }
+  updateStatistics(0, 0, 0, 0)
+  statisticsField.classList.add('hidden')
+  unpublish()
+    .then(restart)
+    .catch(function (error) {
+      console.error('[Red5ProPublisher] :: Error in unpublishing - ' + error)
+      restart()
+    })
+  publishButton.disabled = false
+}
+```
+
+# Publishing
+
+With the `mediaConstraints` object updated with the target camera, the initialization configuration is set to start a new publishing session using the `onGetUserMedia` override to ensure proper listing of devices and labels:
+
+```js
+var config = Object.assign(
+  {},
+  configuration,
+  defaultConfiguration,
+  getAuthenticationParams(),
+  {
+    onGetUserMedia: function () {
+      return new Promise(function (resolve, reject) {
+        if (targetPublisher && targetPublisher.getMediaStream()) {
+          targetPublisher
+            .getMediaStream()
+            .getTracks()
+            .forEach(function (track) {
+              track.stop()
+            })
+        }
+        var stream
+        var constraints = mediaConstraints
+        navigator.mediaDevices
+          .getUserMedia(constraints)
+          .then(function (mediastream) {
+            stream = mediastream
+            return navigator.mediaDevices.enumerateDevices()
+          })
+          .then(function (devices) {
+            listDevices(devices)
+            stream.getVideoTracks().forEach(function (track) {
+              cameraSelect.value = track.getSettings().deviceId
+            })
+            resolve(stream)
+          })
+          .catch(function (error) {
+            reject(error)
+          })
+      })
+    },
+  }
+)
+
+var rtcConfig = {
+  ...config,
+  ...{
+    protocol: getSocketLocationFromProtocol().protocol,
+    port: getSocketLocationFromProtocol().port,
+    streamName: config.stream1,
+  },
+}
+return new red5prosdk.WHIPClient().init(rtcConfig)
+```
+
+> More information: [Media.getUserMedia from MDN](https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia)
---
+++ src/page/test/publishCodecSupport/index.js
@@ -0,0 +1,277 @@
+/*
+Copyright © 2015 Infrared5, Inc. All rights reserved.
+
+The accompanying code comprising examples for use solely in conjunction with Red5 Pro (the "Example Code")
+is  licensed  to  you  by  Infrared5  Inc.  in  consideration  of  your  agreement  to  the  following
+license terms  and  conditions.  Access,  use,  modification,  or  redistribution  of  the  accompanying
+code  constitutes your acceptance of the following license terms and conditions.
+
+Permission is hereby granted, free of charge, to you to use the Example Code and associated documentation
+files (collectively, the "Software") without restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The Software shall be used solely in conjunction with Red5 Pro. Red5 Pro is licensed under a separate end
+user  license  agreement  (the  "EULA"),  which  must  be  executed  with  Infrared5,  Inc.
+An  example  of  the EULA can be found on our website at: https://account.red5.net/assets/LICENSE.txt.
+
+The above copyright notice and this license shall be included in all copies or portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,  INCLUDING  BUT
+NOT  LIMITED  TO  THE  WARRANTIES  OF  MERCHANTABILITY, FITNESS  FOR  A  PARTICULAR  PURPOSE  AND
+NONINFRINGEMENT.   IN  NO  EVENT  SHALL INFRARED5, INC. BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN  AN  ACTION  OF  CONTRACT,  TORT  OR  OTHERWISE,  ARISING  FROM,  OUT  OF  OR  IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+;(function (window, document, red5prosdk) {
+  'use strict'
+
+  let serverSettings = (function () {
+    const settings = sessionStorage.getItem('r5proServerSettings')
+    try {
+      return JSON.parse(settings)
+    } catch (e) {
+      console.error(
+        'Could not read server settings from sessionstorage: ' + e.message
+      )
+    }
+    return {}
+  })()
+
+  let configuration = (function () {
+    const conf = sessionStorage.getItem('r5proTestBed')
+    try {
+      return JSON.parse(conf)
+    } catch (e) {
+      console.error(
+        'Could not read testbed configuration from sessionstorage: ' + e.message
+      )
+    }
+    return {}
+  })()
+  red5prosdk.setLogLevel(
+    configuration.verboseLogging
+      ? red5prosdk.LOG_LEVELS.TRACE
+      : red5prosdk.LOG_LEVELS.WARN
+  )
+
+  let targetPublisher
+
+  const updateStatusFromEvent = window.red5proHandlePublisherEvent // defined in src/template/partial/status-field-publisher.hbs
+  const streamTitle = document.getElementById('stream-title')
+  const statisticsField = document.getElementById('statistics-field')
+  const videoSelect = document.getElementById('video-select')
+  const audioSelect = document.getElementById('audio-select')
+  const publishButton = document.getElementById('publish-button')
+  const bitrateField = document.getElementById('bitrate-field')
+  const packetsField = document.getElementById('packets-field')
+  const resolutionField = document.getElementById('resolution-field')
+
+  let bitrate = 0
+  let packetsSent = 0
+  let frameWidth = 0
+  let frameHeight = 0
+
+  const updateStatistics = (b, p, w, h) => {
+    statisticsField.classList.remove('hidden')
+    bitrateField.innerText = b === 0 ? 'N/A' : Math.floor(b)
+    packetsField.innerText = p
+    resolutionField.innerText = (w || 0) + 'x' + (h || 0)
+  }
+
+  const onBitrateUpdate = (b, p) => {
+    bitrate = b
+    packetsSent = p
+    updateStatistics(bitrate, packetsSent, frameWidth, frameHeight)
+  }
+
+  const onResolutionUpdate = (w, h) => {
+    frameWidth = w
+    frameHeight = h
+    updateStatistics(bitrate, packetsSent, frameWidth, frameHeight)
+  }
+
+  streamTitle.innerText = configuration.stream1
+
+  const protocol = serverSettings.protocol
+  const isSecure = protocol == 'https'
+  const getSocketLocationFromProtocol = () => {
+    return !isSecure
+      ? { protocol: 'ws', port: serverSettings.wsport }
+      : { protocol: 'wss', port: serverSettings.wssport }
+  }
+
+  let defaultConfiguration = {
+    protocol: getSocketLocationFromProtocol().protocol,
+    port: getSocketLocationFromProtocol().port,
+    streamMode: configuration.recordBroadcast ? 'record' : 'live',
+  }
+
+  const mediaConstraints = {
+    audio: configuration.useAudio
+      ? configuration.mediaConstraints.audio
+      : false,
+    video: configuration.useVideo
+      ? configuration.mediaConstraints.video
+      : false,
+  }
+
+  const onPublisherEvent = (event) => {
+    const { type } = event
+    console.log('[Red5ProPublisher] ' + type + '.')
+    updateStatusFromEvent(event)
+  }
+  const onPublishFail = (message) => {
+    console.error('[Red5ProPublisher] Publish Error :: ' + message)
+  }
+  const onPublishSuccess = (publisher) => {
+    console.log('[Red5ProPublisher] Publish Complete.')
+    try {
+      const pc = publisher.getPeerConnection()
+      const stream = publisher.getMediaStream()
+      window.trackBitrate(pc, onBitrateUpdate, onResolutionUpdate)
+      statisticsField.classList.remove('hidden')
+      stream.getVideoTracks().forEach((track) => {
+        var settings = track.getSettings()
+        onResolutionUpdate(settings.width, settings.height)
+      })
+      publishButton.disabled = true
+    } catch (e) {
+      // no tracking for you!
+    }
+  }
+  const onUnpublishFail = (message) => {
+    console.error('[Red5ProPublisher] Unpublish Error :: ' + message)
+  }
+  const onUnpublishSuccess = () => {
+    console.log('[Red5ProPublisher] Unpublish Complete.')
+    publishButton.disabled = false
+  }
+
+  const getAuthenticationParams = () => {
+    const auth = configuration.authentication
+    return auth && auth.enabled
+      ? {
+          connectionParams: {
+            username: auth.username,
+            password: auth.password,
+            token: auth.token,
+          },
+        }
+      : {}
+  }
+
+  const getUniqueCodecListing = (codecType) => {
+    const { PublishVideoEncoder, PublishAudioEncoder } = red5prosdk
+    const encoder =
+      codecType === 'video' ? PublishVideoEncoder : PublishAudioEncoder
+    let capabilities = RTCRtpSender.getCapabilities(codecType)
+    let codecs = capabilities.codecs.map((codec) =>
+      codec.mimeType.match(new RegExp(codecType + '/(.*)'))[1].toUpperCase()
+    )
+    codecs = codecs.filter((codec) => {
+      return encoder[codec]
+    })
+    codecs = codecs
+      .filter((value, index, self) => {
+        return self.indexOf(value) === index
+      })
+      .sort()
+    return codecs
+  }
+
+  const listCodecs = () => {
+    let videoCodecs = getUniqueCodecListing('video')
+    let audioCodecs = getUniqueCodecListing('audio')
+    const videoOptions = videoCodecs.map((codec) => {
+      return `<option value="${codec}">${codec}</option>`
+    })
+    const audioOptions = audioCodecs.map((codec) => {
+      return `<option value="${codec}">${codec}</option>`
+    })
+    videoOptions.unshift('<option value="default" selected>Default</option>')
+    audioOptions.unshift('<option value="default" selected>Default</option>')
+
+    videoSelect.innerHTML = videoOptions.join(' ')
+    audioSelect.innerHTML = audioOptions.join(' ')
+  }
+
+  const startPublishSession = async (mediaStream) => {
+    const { stream1: streamName, preferWhipWhep } = configuration
+    const {
+      WHIPClient,
+      RTCPublisher,
+      PublishAudioEncoder,
+      PublishVideoEncoder,
+    } = red5prosdk
+    const { protocol, port } = getSocketLocationFromProtocol()
+    const rtcConfig = {
+      ...configuration,
+      ...defaultConfiguration,
+      ...getAuthenticationParams(),
+      protocol,
+      port,
+      streamName,
+      videoEncoding:
+        videoSelect.value === 'default'
+          ? undefined
+          : PublishVideoEncoder[videoSelect.value.toUpperCase()],
+      audioEncoding:
+        audioSelect.value === 'default'
+          ? undefined
+          : PublishAudioEncoder[audioSelect.value.toUpperCase()],
+    }
+    try {
+      targetPublisher = preferWhipWhep ? new WHIPClient() : new RTCPublisher()
+      await targetPublisher.initWithStream(rtcConfig, mediaStream)
+      await targetPublisher.publish()
+      onPublishSuccess(targetPublisher)
+    } catch (error) {
+      var jsonError =
+        typeof error === 'string' ? error : JSON.stringify(error, null, 2)
+      console.error(jsonError)
+      onPublishFail(jsonError)
+    }
+  }
+
+  const unpublish = async () => {
+    try {
+      if (targetPublisher) {
+        targetPublisher.off('*', onPublisherEvent)
+        await targetPublisher.unpublish()
+        onUnpublishSuccess()
+      }
+    } catch (error) {
+      var jsonError =
+        typeof error === 'string' ? error : JSON.stringify(error, 2, null)
+      onUnpublishFail('Unmount Error ' + jsonError)
+    } finally {
+      targetPublisher = undefined
+    }
+  }
+
+  const start = async () => {
+    const mediaStream = await navigator.mediaDevices.getUserMedia(
+      mediaConstraints
+    )
+    document.querySelector('#red5pro-publisher').srcObject = mediaStream
+    listCodecs()
+
+    publishButton.disabled = false
+    publishButton.addEventListener('click', () => {
+      startPublishSession(mediaStream)
+    })
+  }
+
+  let shuttingDown = false
+  const shutdown = async () => {
+    if (shuttingDown) return
+    shuttingDown = true
+    window.untrackBitrate()
+    await unpublish()
+  }
+  window.addEventListener('pagehide', shutdown)
+  window.addEventListener('beforeunload', shutdown)
+
+  start()
+})(this, document, window.red5prosdk)
