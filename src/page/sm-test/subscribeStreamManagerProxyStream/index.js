2fc6d40bb42be442b74f2ed11c7dc770a6875004
Todd Anderson <bustardcelly@gmail.com>
2026-01-07 10:52:48 -0500

    wip: test of stream assignment to element.

---
+++ src/page/sm-test/subscribeStreamManagerProxyStream/index.js
@@ -0,0 +1,346 @@
+/*
+Copyright © 2015 Infrared5, Inc. All rights reserved.
+
+The accompanying code comprising examples for use solely in conjunction with Red5 Pro (the "Example Code")
+is  licensed  to  you  by  Infrared5  Inc.  in  consideration  of  your  agreement  to  the  following
+license terms  and  conditions.  Access,  use,  modification,  or  redistribution  of  the  accompanying
+code  constitutes your acceptance of the following license terms and conditions.
+
+Permission is hereby granted, free of charge, to you to use the Example Code and associated documentation
+files (collectively, the "Software") without restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The Software shall be used solely in conjunction with Red5 Pro. Red5 Pro is licensed under a separate end
+user  license  agreement  (the  "EULA"),  which  must  be  executed  with  Infrared5,  Inc.
+An  example  of  the EULA can be found on our website at: https://account.red5.net/assets/LICENSE.txt.
+
+The above copyright notice and this license shall be included in all copies or portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,  INCLUDING  BUT
+NOT  LIMITED  TO  THE  WARRANTIES  OF  MERCHANTABILITY, FITNESS  FOR  A  PARTICULAR  PURPOSE  AND
+NONINFRINGEMENT.   IN  NO  EVENT  SHALL INFRARED5, INC. BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN  AN  ACTION  OF  CONTRACT,  TORT  OR  OTHERWISE,  ARISING  FROM,  OUT  OF  OR  IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+;(function (window, document, red5prosdk) {
+  'use strict'
+
+  const configuration = (function () {
+    const conf = sessionStorage.getItem('r5proTestBed')
+    try {
+      return JSON.parse(conf)
+    } catch (e) {
+      console.error(
+        'Could not read testbed configuration from sessionstorage: ' + e.message
+      )
+    }
+    return {}
+  })()
+  red5prosdk.setLogLevel(
+    configuration.verboseLogging
+      ? red5prosdk.LOG_LEVELS.TRACE
+      : red5prosdk.LOG_LEVELS.WARN
+  )
+
+  var targetSubscriber
+
+  const updateStatusFromEvent = event => {
+    const subTypes = red5prosdk.SubscriberEventTypes
+    switch (event.type) {
+      case subTypes.CONNECT_FAILURE:
+      case subTypes.SUBSCRIBE_FAIL:
+        shutdownVideoElement()
+        break
+    }
+    window.red5proHandleSubscriberEvent(event) // defined in src/template/partial/status-field-subscriber.hbs
+  }
+
+  const instanceId = Math.floor(Math.random() * 0x10000).toString(16)
+  const streamTitle = document.getElementById('stream-title')
+  const statisticsField = document.getElementById('statistics-field')
+  const bitrateField = document.getElementById('bitrate-field')
+  const packetsField = document.getElementById('packets-field')
+  const resolutionField = document.getElementById('resolution-field')
+  const addressField = document.getElementById('address-field')
+
+  let bitrate = 0
+  let packetsReceived = 0
+  let frameWidth = 0
+  let frameHeight = 0
+
+  const updateStatistics = (b, p, w, h) => {
+    statisticsField.classList.remove('hidden')
+    bitrateField.innerText = b === 0 ? 'N/A' : Math.floor(b)
+    packetsField.innerText = p
+    resolutionField.innerText = (w || 0) + 'x' + (h || 0)
+  }
+
+  const onBitrateUpdate = (b, p) => {
+    bitrate = b
+    packetsReceived = p
+    updateStatistics(bitrate, packetsReceived, frameWidth, frameHeight)
+  }
+
+  const onResolutionUpdate = (w, h) => {
+    frameWidth = w
+    frameHeight = h
+    updateStatistics(bitrate, packetsReceived, frameWidth, frameHeight)
+  }
+
+  const defaultConfiguration = ((useVideo, useAudio) => {
+    let c = configuration
+    if (!useVideo) {
+      c.videoEncoding = red5prosdk.PlaybackVideoEncoder.NONE
+    }
+    if (!useAudio) {
+      c.audioEncoding = red5prosdk.PlaybackAudioEncoder.NONE
+    }
+    return c
+  })(configuration.useVideo, configuration.useAudio)
+
+  const shutdownVideoElement = () => {
+    const videoElement = document.getElementById('red5pro-subscriber')
+    if (videoElement) {
+      videoElement.pause()
+      videoElement.src = ''
+    }
+  }
+
+  const getAuthenticationParams = () => {
+    const { authentication } = configuration
+    const { enabled, username, password, token } = authentication
+    return enabled
+      ? {
+          connectionParams: {
+            username,
+            password,
+            token
+          }
+        }
+      : {}
+  }
+
+  const displayServerAddress = (serverAddress, proxyAddress) => {
+    addressField.classList.remove('hidden')
+    proxyAddress = typeof proxyAddress === 'undefined' ? 'N/A' : proxyAddress
+    addressField.innerText = `Proxy Address: ${proxyAddress} | Edge Address: ${serverAddress}`
+  }
+
+  // Assume these are declared outside of the event handler?
+  // const remoteVideo = document.getElementById('red5pro-subscriber')
+  // const remoteStream = new MediaStream()
+  // remoteVideo.srcObject = remoteStream
+
+  // Local lifecycle notifications.
+  const onSubscriberEvent = event => {
+    const { type } = event
+    const { data } = event
+    const remoteTracks = {
+      videoTrack: null,
+      audioTrack: null
+    }
+    const remoteVideo = document.getElementById('red5pro-subscriber')
+    const remoteStream = new MediaStream()
+
+    console.log('[Red5ProSubsriber] ' + type + '.')
+    updateStatusFromEvent(event)
+    if (type === 'Subscribe.VideoDimensions.Change') {
+      onResolutionUpdate(event.data.width, event.data.height)
+    } else if (type === 'WebRTC.Endpoint.Changed') {
+      const { host } = configuration
+      const { data } = event
+      const { endpoint } = data
+      displayServerAddress(endpoint, host)
+    }
+
+    const attachMediaStream = (element, stream) => {
+      console.log('[OF] ATTACH')
+      // if (element.srcObject) {
+      //   console.log('[OF] SRC OBJECT', element.srcObject)
+      //   return
+      // }
+      try {
+        element.srcObject = stream
+      } catch (e) {
+        try {
+          element.src = URL.createObjectURL(stream)
+        } catch (e) {
+          console.log('[OF] Error attaching stream to element', e, [
+            'RED5',
+            'ERROR'
+          ])
+        }
+      }
+      try {
+        element.play()
+      } catch (e) {
+        console.log('[OF] Error playing element', e, ['RED5', 'ERROR'])
+      }
+    }
+    if (type === 'WebRTC.PeerConnection.OnTrack') {
+      const mediaType = data.track.kind
+      console.log('[OF] ON TRACK', mediaType)
+      // const { streams } = data
+      // streams.forEach(stream => {
+      //   attachMediaStream(remoteVideo, stream)
+      // })
+      if (mediaType === 'video') {
+        remoteTracks.videoTrack = data.track
+
+        if (remoteTracks.videoTrack) {
+          remoteStream.addTrack(remoteTracks.videoTrack)
+        }
+        attachMediaStream(remoteVideo, remoteStream)
+      }
+      if (mediaType === 'audio') {
+        remoteTracks.audioTrack = data.track
+
+        if (remoteTracks.audioTrack) {
+          remoteStream.addTrack(remoteTracks.audioTrack)
+        }
+      }
+    }
+  }
+  const onSubscribeFail = message => {
+    console.error('[Red5ProSubsriber] Subscribe Error :: ' + message)
+  }
+  const onSubscribeSuccess = subscriber => {
+    console.log('[Red5ProSubsriber] Subscribe Complete.')
+    if (window.exposeSubscriberGlobally) {
+      window.exposeSubscriberGlobally(subscriber)
+    }
+    if (subscriber.getType().toLowerCase() === 'rtc') {
+      try {
+        window.trackBitrate(
+          subscriber.getPeerConnection(),
+          onBitrateUpdate,
+          onResolutionUpdate,
+          true
+        )
+      } catch (e) {
+        //
+      }
+    }
+  }
+  const onUnsubscribeFail = message => {
+    console.error('[Red5ProSubsriber] Unsubscribe Error :: ' + message)
+  }
+  const onUnsubscribeSuccess = () => {
+    console.log('[Red5ProSubsriber] Unsubscribe Complete.')
+  }
+
+  const getRegionIfDefined = () => {
+    const region = configuration.streamManagerRegion
+    if (
+      typeof region === 'string' &&
+      region.length > 0 &&
+      region !== 'undefined'
+    ) {
+      return region
+    }
+    return undefined
+  }
+
+  const getConfiguration = () => {
+    const {
+      host,
+      app,
+      protocol,
+      port,
+      stream1,
+      streamManagerAPI,
+      streamManagerNodeGroup: nodeGroup
+    } = configuration
+
+    const region = getRegionIfDefined()
+    const params = region
+      ? {
+          region,
+          strict: true
+        }
+      : undefined
+
+    const httpProtocol = protocol === 'ws' ? 'http' : 'https'
+    const endpoint = `${httpProtocol}://${host}:${port}/as/${streamManagerAPI}/proxy/whep/${app}/${stream1}`
+
+    var connectionParams = params
+      ? { ...params, ...getAuthenticationParams().connectionParams }
+      : getAuthenticationParams().connectionParams
+
+    var rtcConfig = {
+      ...configuration,
+      ...defaultConfiguration,
+      endpoint,
+      streamName: stream1,
+      subscriptionId: 'subscriber-' + instanceId,
+      mediaElementId: undefined,
+      connectionParams: {
+        ...connectionParams,
+        nodeGroup
+      }
+    }
+    return rtcConfig
+  }
+
+  const startSubscriber = async () => {
+    try {
+      const { WHEPClient } = red5prosdk
+      const { stream1 } = configuration
+      const config = getConfiguration()
+      const subscriber = new WHEPClient()
+      subscriber.on('*', onSubscriberEvent)
+      await subscriber.init(config)
+      await subscriber.subscribe()
+      onSubscribeSuccess(subscriber)
+      streamTitle.innerText = stream1
+      targetSubscriber = subscriber
+    } catch (error) {
+      var jsonError =
+        typeof error === 'string' ? error : JSON.stringify(error, null, 2)
+      console.error(
+        '[Red5ProSubscriber] :: Error in access of Edge IP: ' + jsonError
+      )
+      updateStatusFromEvent({
+        type: red5prosdk.SubscriberEventTypes.CONNECT_FAILURE
+      })
+      onSubscribeFail(jsonError)
+    }
+  }
+
+  // Request to unsubscribe.
+  const unsubscribe = async () => {
+    try {
+      var subscriber = targetSubscriber
+      await subscriber.unsubscribe()
+      onUnsubscribeSuccess()
+    } catch (error) {
+      var jsonError =
+        typeof error === 'string' ? error : JSON.stringify(error, 2, null)
+      onUnsubscribeFail('Unmount Error ' + jsonError)
+      throw error
+    }
+  }
+
+  // Clean up
+  let shuttingDown = false
+  const shutdown = async () => {
+    if (shuttingDown) return
+    shuttingDown = true
+    try {
+      await unsubscribe()
+    } catch (e) {
+      console.error(e)
+    } finally {
+      if (targetSubscriber) {
+        targetSubscriber.off('*', onSubscriberEvent)
+      }
+      targetSubscriber = undefined
+    }
+    window.untrackBitrate()
+  }
+  window.addEventListener('pagehide', shutdown)
+  window.addEventListener('beforeunload', shutdown)
+
+  startSubscriber()
+})(this, document, window.red5prosdk)
